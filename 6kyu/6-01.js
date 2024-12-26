73c0a4bf2c920b02cd9ce325204436b1d2b86849
jamesbatayola <batayolajakejames@gmail.com>
2024-12-26 16:15:20 +0800

    6-01

--- 6kyu/6-01.js
+++ 6kyu/6-01.js
@@ -1,57 +1,24 @@
-// The main part of this game is merging identical tiles in a row.
+// The Pell sequence is the sequence of integers defined by the initial values
 
-//     Implement a function that models the process of merging all of the tile values in a single row.
-//     This function takes the array line as a parameter and returns a new array with the tile values from line slid towards the front of the array (index 0) and merged.
-//     A given tile can only merge once.
-//     Empty grid squares are represented as zeros.
-//     Your function should work on arrays containing arbitrary number of elements.
+// P(0) = 0, P(1) = 1
 
-// Examples
+// and the recurrence relation
 
-// merge([2,0,2,2])  -->  [4,2,0,0]
+// P(n) = 2 * P(n-1) + P(n-2)
 
-// Another example with repeated merges:
+// The first few values of P(n) are
 
-// merge([4,4,8,16])  -->  [8,8,16,0]
-// merge([8,8,16,0])  -->  [16,16,0,0]
-// merge([16,16,0,0]) -->  [32,0,0,0]
+// 0, 1, 2, 5, 12, 29, 70, 169, 408, 985, 2378, 5741, 13860, 33461, 80782, 195025, 470832, ..
 
-// function merge(line) {
-//     const res = [];
-//     line.sort();
-//     for(let i = 1; i < line.length; i++) {
-//         if(line[i] > 0 && line[i-1] > 0 && line[i] === line[i-1]) {
-//             res.push(line[i] * 2);
-//         } else {
-//             res.push(line[i]);
-//         }
-//     }
-//     for(let i = 0; i <= line.length - res.length; i++) {
-//         res.push(0);
-//     }
-//     return res;
-// };
+function pell(n) {
+   const pell = [0n, 1n];
 
-// -UNFINISHED-
+   for (let i = 1; i < n; i++) {
+      const curr = pell[i] * 2n + pell[i - 1];
+      pell.push(BigInt(curr));
+   }
 
-function merge(line) {
-    const res = [];
-    let copy = [...line];
-    for(let i = 0; i < copy.length;) {
-        for(let j = 1; j < copy.length; j++) {
-            if(copy[i] === copy[j]) {
-                res.push(copy[i] * 2);
-                copy = copy.filter(e => e !== copy[i]);
-                i = 0;
-            } else {
-                res.push(copy[j])
-                i++;
-            }
-        }
-    }
-    return res;
-};
+   return pell[n];
+}
 
-const test1 = merge([2,0,2,2]);
-
-test1.forEach(e => console.log(e));
+console.log(pell(4));
