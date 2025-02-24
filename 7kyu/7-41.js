b188bd895d7ae4ee036b70ce12fed296c6cd65f2
jamesbatayola <batayolajakejames@gmail.com>
2025-02-24 10:09:06 +0800

    7-41

---
+++ 7kyu/7-41.js
@@ -0,0 +1,13 @@
+// Create a function that accepts an array of names, and returns an array of each name with its first letter capitalized and the remainder in lowercase.
+// Examples
+
+// ['jo', 'nelson', 'jurie'] -->  ['Jo', 'Nelson', 'Jurie']
+// ['KARLY', 'DANIEL', 'KELSEY'] --> ['Karly', 'Daniel', 'Kelsey']
+
+function capMe(names) {
+   return names.map(
+      (name) => name[0].toUpperCase() + name.slice(1).toLowerCase()
+   );
+}
+
+console.log(capMe(["jo", "nelson", "jurie"]));
