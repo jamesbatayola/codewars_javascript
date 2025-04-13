ddf3795745792e68a9edb01a1229cae4a8e12594
jamesbatayola <batayolajakejames@gmail.com>
2025-04-13 14:56:27 +0800

    7-54

--- 5kyu/5-02.js
+++ 5kyu/5-02.js
@@ -15,9 +15,6 @@
 
 // Notes and tips: using the solution to the other kata to check your function may be helpful, but as much larger numbers will be used, using an array/list to compute the number of the survivor may be too slow; you may assume that both n and k will always be >=1.
 
-
-
-
 function josephusSurvivor(n,k){
     const arr = [];
 
